c4ecba58b5c4058265eb548dbb76ef33188dd560
Aleksei Makarov <aleksey.makarov@gmail.com>
2026-05-13 23:00:00 +0200

    Collect reports from fs-object roots
    Teach the test-reports collector to copy report files from the
    root directory of sandbox result objects mounted as Binary inputs.

--- tests/test-reports.sh
+++ tests/test-reports.sh
@@ -7,13 +7,22 @@ mkdir -p "$dest"
 
 copied=0
 
-for input_dir in /__mbuild/inputs/report*; do
-  [ -d "$input_dir" ] || continue
+copy_reports_from_dir() {
+  local report_dir="$1"
+
+  [ -d "$report_dir" ] || return
 
   while IFS= read -r -d '' report; do
     cp "$report" "$dest/"
     copied=$((copied + 1))
-  done < <(find "$input_dir" -maxdepth 1 -type f -name 'report-*.txt' -print0 | sort -z)
+  done < <(find "$report_dir" -maxdepth 1 -type f -name 'report-*.txt' -print0 | sort -z)
+}
+
+for input_dir in /__mbuild/inputs/report*; do
+  [ -d "$input_dir" ] || continue
+
+  copy_reports_from_dir "$input_dir"
+  copy_reports_from_dir "$input_dir/root"
 done
 
 printf '%s\n' "$copied" > "$dest/copied-count.txt"
